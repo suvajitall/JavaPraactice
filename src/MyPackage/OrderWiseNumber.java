cdf9f0124e1c33f86b0bfbf3799427806b14c028
suvajitall <suvajitall@gmail.com>
2024-02-26 00:41:27 +0530

    Comment has been added by dev02

--- src/MyPackage/OrderWiseNumber.java
+++ src/MyPackage/OrderWiseNumber.java
@@ -5,6 +5,8 @@ public class OrderWiseNumber {
 		int[] arr = {5,3,4,5,2,0,10,50};
 		int len = arr.length;
 		int temp=arr[0];
+		/****** This code is for ascending order only. For descending order 
+		 * just change  the ">" to "<" at the line number 12****/
 		for(int i=0;i<len;i++) {
 			for(int j=i+1;j<len;j++) {
 				if(arr[i]>arr[j]) {
